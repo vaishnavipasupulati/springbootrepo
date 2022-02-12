2614c641092e8defa6e31535165fc0886a878a30
vaishnavipasupulati <98844705+vaishnavipasupulati@users.noreply.github.com>
2022-02-12 11:58:07 -0500

    Update FirstspringbootApplication.java

--- firstspringboot/src/main/java/com/example/firstspringboot/FirstspringbootApplication.java
+++ firstspringboot/src/main/java/com/example/firstspringboot/FirstspringbootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstspringbootApplication {
 
 	public static void main(String[] args) {
+		//Coment//
 		SpringApplication.run(FirstspringbootApplication.class, args);
 	}
 
